c67c21b89f3491e874f4111e5f30a2ec6e8523ce
= <dariocabrera@tdf.edu.ar>
2023-11-04 15:17:18 -0300

    controller ya compatible con el front

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -1,34 +1,44 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, Query } from '@nestjs/common';
 import { PetService } from './pet.service';
 import { PetDto } from './dto/create-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
 
-@Controller('mascota')
+@Controller('pets')
 export class PetController {
   constructor(private readonly petService: PetService) {}
 
-  @Post('nuevo')
-  async createNewPet(@Body() createPetDto: PetDto):Promise<PetDto> {
-    return await this.petService.createNewPet(createPetDto);
+  // @Post('nuevo')
+  // async createNewPet(@Body() createPetDto: PetDto):Promise<PetDto> {
+  //   return await this.petService.createNewPet(createPetDto);
+  // }
+
+  // @Get()
+  // async findAllPet():Promise<PetDto[]> {
+  //   return await this.petService.findAll();
+  // }
+
+  @Get('count')
+    async getCount(): Promise<number> {
+        return await this.petService.countPets();
+    }
+
+  @Get('filter__:pageNumber')
+  async filterPet(@Param('pageNumber') pageNumber: number, @Query('specie') specie?: string, @Query('sex') sex?: string, @Query('location') location?: number  ):Promise<PetDto[]> {
+    return await this.petService.filterPets(pageNumber, specie, location, sex);
   }
 
-  @Get()
-  async findAllPet():Promise<PetDto[]> {
-    return await this.petService.findAll();
-  }
+  // @Get(':id')
+  // async findOnePet(@Param('id') id:number): Promise<PetDto> {
+  //   return await this.petService.findOne(id);
+  // }
 
-  @Get(':id')
-  async findOnePet(@Param('id') id:number): Promise<PetDto> {
-    return await this.petService.findOne(id);
-  }
+  // @Put('modificar/:id')
+  // async updatePet(@Param('id') id: number, @Body() updatePetDto: UpdatePetDto):Promise<PetDto> {
+  //   return await this.petService.update(id, updatePetDto);
+  // }
 
-  @Put('modificar/:id')
-  async updatePet(@Param('id') id: number, @Body() updatePetDto: UpdatePetDto):Promise<PetDto> {
-    return await this.petService.update(id, updatePetDto);
-  }
-
-  @Delete('eliminar/:id')
-  async removePet(@Param('id') id:number):Promise<string> {
-    return await this.petService.remove(id);
-  }
+  // @Delete('eliminar/:id')
+  // async removePet(@Param('id') id:number):Promise<string> {
+  //   return await this.petService.remove(id);
+  //}
 }
