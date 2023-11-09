be5233fa090fc8019ccd881ee5a92862494ffa00
= <dariocabrera@tdf.edu.ar>
2023-11-09 16:39:04 -0300

    varieable .env hardcodeada, buscar solucion

--- env.vars.ts
+++ env.vars.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 export const database = process.env.database;
 export const password = process.env.password;
-export const username = process.env.username;
+export const username = 'root'; //no lee la env variable :/
 export const port = Number(process.env.port);
 export const host = process.env.host;
